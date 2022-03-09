ba80365852f4d9ebd067c4e93373b49424a0081f
谭新棋 <652605571@qq.com>
2022-03-09 16:04:06 +0800

    移除无用功能

--- TXQ.Utils.Test/Base64Test.cs
+++ TXQ.Utils.Test/Base64Test.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TXQ.Utils.Tool;
 
 namespace TXQ.Utils.Test
 {
@@ -9,8 +10,9 @@ namespace TXQ.Utils.Test
         public void Base64Test1()
         {
             var file = System.IO.Path.GetTempFileName();
-        //    System.IO.File.WriteAllText(file,)
+            //    System.IO.File.WriteAllText(file,)
 
+            System.Console.WriteLine(new System.IO.FileInfo(@"E:\Source\Downloads\NetDrive3_Setup-3.15.450.exe.torrent").EXGetBase64());
         }
     }
 }
